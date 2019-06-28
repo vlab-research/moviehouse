e8ba1af3cce72b220e3e847f74f767acaa4492f3
bertacume <bertacume@gmail.com>
2019-06-28 11:03:53 +0200

    Feat/userId (#4)
    * chore: install messenger sdk
    
    * feat: post psid to server
    
    * refactor: add getContext
    
    * fix: video unavailable without user id
    
    * fix: body selector
    
    * style: responsive video and dark bg
    
    * style: handle error
    
    * style: center error elements
    
    * fix: get videoId from params
    
    * feat: added handle close webview
    
    * refactor: log error on close webview
    
    * refactor: skip new line and handle end event

--- src/script.js
+++ src/script.js
@@ -2,39 +2,68 @@
 
 const SERVER_URL = '{{{SERVER_URL}}}';
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const videoId = params.get('id');
+let psid;
 
 function handleEvent(data, eventType) {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', SERVER_URL);
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({ data, eventType }));
+  xhr.send(JSON.stringify({ data, eventType, psid }));
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+function handleError(err, title, message) {
+  const div = document.createElement('div');
+  div.classList.add("error-container");
+  div.innerHTML = `<h1>${title}</h1><p>${message}</p>`;
+  document.querySelector('.container').innerHTML = ``
+  document.querySelector('.container').appendChild(div);
+  console.error(err);
+}
+
+function setPlayer() {
   const options = {
-    id,
-    width: 800
+    id: videoId,
+    responsive: true
   };
+
   const player = new Vimeo.Player('vimeoVideo', options);
 
   player.ready().then(() => {
-    player.on('ended', data => handleEvent(data, 'ended'));
-  
+    player.on('ended', data => {
+      MessengerExtensions.requestCloseBrowser(
+        function success() {
+          handleEvent(data, 'ended');
+        }, function error(err) {
+          console.error(err);
+        }
+      );
+    });
     player.on('error', data => handleEvent(data, 'error'));
-    
     player.on('pause', data => handleEvent(data, 'pause'));
-    
     player.on('play', data => handleEvent(data, 'play'));
-    
     player.on('playbackratechange', data => handleEvent(data, 'playbackratechange'));
-    
     player.on('seeked', data => handleEvent(data, 'seeked'));
-  
     player.on('volumechange', data => handleEvent(data, 'volumechange'));
   }).catch((err) => {
-    console.error(err);
-    document.querySelector('body').innerHTML = `<p>Video not found</p>`;
+    const title = '❌ Not found';
+    const message = 'Sorry, we couldn’t find that video'
+    handleError(err, title, message);
   });
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+  window.extAsyncInit = function () {
+    MessengerExtensions.getContext('{{{APP_ID}}}',
+      function success(thread_context) {
+        psid = thread_context.psid;
+        setPlayer();
+      },
+      function error(err) {
+        const title = '🔒Forbidden';
+        const message = 'It seems you are not logged in with facebook'
+        handleError(err, title, message);
+      }
+    );
+  };
 });
